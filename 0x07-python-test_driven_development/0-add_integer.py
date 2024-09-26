3a3a7d0b19969fb76d40e4bdcbe321e3bd88cfb6
Shallom4nv <oluwaseun.james@gmail.com>
2024-09-26 16:08:29 -0700

    0-add_integer.py push

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+"""
+Module o-add_integer
+contain one method and accept two values of int or float type
+and cast them to int before adding
+Reture an int sum
+"""
+def add_integer(a, b=98):
+    """
+    Return the summation of two argument
+    """
+    if type(a) is not int and type(a is not float:
+        raise TypeError("b must be an interger")
+    elif type(b) is not int and type(b) is not float:
+         raise TypeError("b must be an integer")
+    else:
+        if type(a) is float:
+            a = int(a)
+        if typs(b) is float:
+            b - int(b)
+        return a +b
+
